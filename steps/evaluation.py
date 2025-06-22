039130686a071b3cd587db3fab04890faf0d206e
Obsa Sendaba <=obsa.abate@slu.edu>
2025-06-22 13:25:19 -0500

    eval_step

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -1,12 +1,35 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.evaluation import Evaluation, Accuracy, R2, MSE
+from sklearn.base import RegressorMixin
+from typing_extensions import Tuple
+from typing import Annotated
 
 @step
-def evaluate_model(df:pd.DataFrame) -> None:
+def evaluate_model(model: RegressorMixin, X_test:pd.DataFrame, 
+                   y_test:pd.Series) -> Tuple[
+                       Annotated[float, "mse_class"],
+                       Annotated[float, "acc_class"],
+                       Annotated[float, "r2_score"]
+                   ]:
     """
     Evaluate the model on the ingested dataframe.
     Args:
-        df:the ingested data
+        Model: RegressorMixin, 
+        X_test:pd.DataFrame, 
+        y_test:pd.Series
+     Returns:
+
     """
-    pass
+    try:
+        prediction = model.predict(X_test)
+        mse_class = MSE()
+        mse_class.calculate_scores(y_test=y_test, y_pred=prediction)
+        acc_class = Accuracy()
+        acc_class.calculate_scores(y_test=y_test, y_pred=prediction)
+        r2_score =  R2()
+        r2_score.calculate_scores(y_test=y_test, y_pred=prediction)
+        return mse_class, acc_class, r2_score
+    except Exception as e:
+        logging.error("Error in Evaluating model {}".format(e))
