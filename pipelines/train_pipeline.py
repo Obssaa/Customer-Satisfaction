b27c5a328f004305494a1cd3276a89ae332743b6
Obsa Sendaba <=obsa.abate@slu.edu>
2025-06-23 16:27:19 -0500

    train_pipeline

--- pipelines/train_pipeline.py
+++ pipelines/train_pipeline.py
@@ -10,7 +10,7 @@ def train_pipeline(data_path:str):
     create the pipeline from steps
     """
     df = ingest_data(data_path=data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
-
+    X_train, X_test, y_train, y_test = clean_df(df)
+    model = train_model(X_train, X_test, y_train, y_test)
+    mse_class, acc_class, r2_score = evaluate_model(model,X_test,y_test)
+    return  mse_class, acc_class, r2_score
