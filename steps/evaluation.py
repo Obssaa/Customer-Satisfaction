0d14197a3bddcd3afb9f5a5bd99feef850f93fc6
Obsa Sendaba <=obsa.abate@slu.edu>
2025-06-24 21:55:05 -0500

    mlflow

--- steps/evaluation.py
+++ steps/evaluation.py
@@ -5,8 +5,14 @@ from src.evaluation import Evaluation, R2, MSE
 from sklearn.base import RegressorMixin
 from typing_extensions import Tuple
 from typing import Annotated
+from zenml.client import Client
 
-@step
+import mlflow
+
+expriment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=expriment_tracker.name)
+
 def evaluate_model(model: RegressorMixin, X_test:pd.DataFrame, 
                    y_test:pd.Series) -> Tuple[
                        Annotated[float, "mse_class"],
@@ -24,7 +30,9 @@ def evaluate_model(model: RegressorMixin, X_test:pd.DataFrame,
     try:
         prediction = model.predict(X_test)
         mse_class = MSE().calculate_scores(y_test, prediction)
+        mlflow.log_metric("mse",mse_class)
         r2_score =  R2().calculate_scores(y_test,prediction)
+        mlflow.log_metric("r2",r2_score)
         return mse_class, r2_score
     except Exception as e:
         logging.error("Error in Evaluating model {}".format(e))
--- steps/train_model.py
+++ steps/train_model.py
@@ -4,8 +4,13 @@ from zenml import step
 from src.model_dev import LinearRegressionModel
 from sklearn.base import RegressorMixin
 from steps.config import ModelNameConfig
+from zenml.client import Client
+import mlflow
 
-@step
+expriment_tracker = Client().active_stack.experiment_tracker
+
+@step(experiment_tracker=expriment_tracker.name)
+
 def train_model(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.DataFrame, 
                 y_test: pd.DataFrame, config:ModelNameConfig) -> RegressorMixin:
 
@@ -22,6 +27,7 @@ def train_model(X_train: pd.DataFrame, X_test: pd.DataFrame, y_train: pd.DataFra
     try:
         model = None
         if config.model_name == "LinearRegression":
+            mlflow.sklearn.autolog()
             model = LinearRegressionModel ()
             trained_model = model. train (X_train, y_train)
             return trained_model
